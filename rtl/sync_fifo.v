38af16651fbfaf64ea2303e44262ef29a32f4a80
Jayanth-Reddy10 <jayanthreddy1007@gmail.com>
2026-02-10 12:18:34 +0530

    Add sync FIFO RTL module

---
+++ rtl/sync_fifo.v
@@ -0,0 +1,5 @@
+// -------------------------------------------------
+// Synchronous FIFO
+// Author: Jayanth Reddy
+// Description: RTL implementation of Sync FIFO
+// -------------------------------------------------
